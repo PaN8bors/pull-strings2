76bbf96895fa4f092c8f5707e76f8f7bbb7ae256
Parish Neighbors <pn8bors@gmail.com>
2022-02-01 15:13:39 -0600

    Second commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function main() {
+    const myMessage = "Hello world!";
+    let result = '';
+
+    for (let i = 0; i < myMessage.length; i += 1) {
+        const char = myMessage[i];
+
+        if ((i % 2) == 0) {
+            result += char.toUpperCase();
+        } else {
+            result += char;
+        }
+    }
+    console.log(result);
+
+    }
+
+
+main();
